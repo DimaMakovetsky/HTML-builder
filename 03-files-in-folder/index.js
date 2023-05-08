707818bbf19b41e410271c120a0fbc2f544d0ddf
DimaMakovetsky <mister.chuppa@mail.ru>
2023-05-08 19:49:34 +0300

    feat: complete task 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const fs = require('fs');
+const pathToFolder = path.basename(__dirname)+'\\secret-folder';
+console.log(pathToFolder);
+fs.readdir(pathToFolder, {withFileTypes:true}, (err, files) => {
+  if(err) {
+    console.log(err);
+  }
+  files.forEach(file => {
+    fs.stat(pathToFolder+"\\"+file.name,(err, fileStats) => {
+      if (err) {
+        console.log(err);
+      }
+      
+      if(!fileStats.isDirectory())
+      {
+        console.log(path.parse(file.name).name + " - " + path.extname(file.name) + " - " + fileStats.size);
+      }
+    })
+  });
+});
