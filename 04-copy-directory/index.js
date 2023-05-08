02c16b55a85cf22df8ffc8f545ba99dd75b8b8f5
DimaMakovetsky <mister.chuppa@mail.ru>
2023-05-08 20:29:26 +0300

    feat: complete task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,32 @@
+const path = require('path');
+const fs = require('fs');
+const pathToFolder = path.basename(__dirname)+'\\files';
+const pathToFolderCopy = path.basename(__dirname)+'\\files-copy'
+fs.mkdir(pathToFolderCopy,{recursive:true},(err)=>{
+  if (err) {
+    console.log(err);
+  }
+});
 
+fs.readdir(pathToFolderCopy, {withFileTypes:true}, (err, files) => {
+  if(err) {
+    console.log(err);
+  }
+  files.forEach(file => {
+    fs.unlink(pathToFolderCopy+"\\"+file.name, () => {
+    });
+  });
+});
+
+fs.readdir(pathToFolder, {withFileTypes:true}, (err, files) => {
+  if(err) {
+    console.log(err);
+  }
+  files.forEach(file => {
+    fs.copyFile(pathToFolder+"\\" + file.name, pathToFolderCopy + "\\" + file.name,(err) => {
+      if (err) {
+        console.log(err);
+      }
+    })
+  });
+});
